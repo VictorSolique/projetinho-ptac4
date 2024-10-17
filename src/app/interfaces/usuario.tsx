f5f95b76e4ea8734c246d9f7440de80dd8a73934
Anabelagarcia <anabelanams@gmail.com>
2024-10-17 14:38:53 -0400

    update

--- src/app/interfaces/usuario.tsx
+++ src/app/interfaces/usuario.tsx
@@ -1,18 +1,8 @@
-interface Usuario {
+export default interface Usuario {
     nome: string;
     idade: number;
     email?: string;
 }
-
-// const PerfilUsuario:React.FC<{usuario: Usuario}> = ({usuario}) => {
-//     return (
-//         <div>
-//             <h1>{usuario.nome}</h1>
-//             <p>{usuario.idade}</p>
-//             {usuario.email && <p>Email: {usuario.email}</p>} 
-//         </div>
-//     )
-// }
 
 // export default PerfilUsuario;
 export default Usuario;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,14 @@ import MyInput from "./components/Input";
 export default function Home() {
   return (
     <div className={styles.main}>
-<<<<<<< HEAD
         <h1>Login Necessário</h1>
         <p>Realize o login para poder acessar o site</p>
         <Button name="Otavio" numero={10} />
         <MyInput value="nkljfd"/>
         <Link href={"/login"} className={styles.link}>Ir para login</Link>
-=======
         <h1>Atividade 2 - PTAC4</h1>
         <p>Três Interfaces</p>
         <Link href={"/perfil"} className={styles.link}>Ir para página Perfil</Link>
->>>>>>> 3e0646a56b0bdd77fc5e358e47da96eb8fa89868
         <p style={{fontSize: "12px"}}>Dupla: Anabela e Victor Solique</p>
     </div>
 
