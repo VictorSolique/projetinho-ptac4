1f3c063c42a20ad120e7620dbf5d5a50abddb9cc
Victor de Souza Solique <victordesouzasolique@gmail.com>
2024-10-24 14:45:32 -0400

    commit 24/10

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { useState } from "react"
 import Usuario from "../interfaces/usuario"
 import { useRouter } from "next/navigation"
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,29 +1,59 @@
 'use client'
 import styles from "../page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Usuario from "../interfaces/usuario";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [error, setError] = useState<string>();
+  const [usuarios, setUsuarios] = useState<Usuario[]>([
+    {
+        id: 1,
+        nome: "Jorginho BeachTenis",
+        email: "jorginho.beachtenis@gmail.com",
+        password: "senha123",
+        tipo: "adm"
+    },
+    {
+        id: 2,
+        nome: "Cela Lopes",
+        email: "cela.lopes@gmail.com",
+        password: "senha123",
+        tipo: "cliente"
+    }
+  ])
   const router = useRouter();
 
-  function verificarLogin() {
-    if (email !== 'ptac4' || password !== 'nota10') {
-      setError('E-mail ou senha inválidos');
-      return;
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const usuario = usuarios.find((user) => user.email == email && user.password == password)
+    if(usuario) {
+      localStorage.setItem('usuario', JSON.stringify(usuario))
+      router.push('/cadastro')
     }
-
-    setError('');
-    router.push('/')
+    else {
+      setError("Email ou senha invalidos")
+    }
+    
   }
+
+  useEffect(() => {
+    const usuarioLogado = localStorage.getItem('usuario')
+    if (usuarioLogado) {
+      router.push('/cadastro')
+    }
+    else {
+      setError("Email ou senha inválido")
+    }
+  }, [router])
 
   return (
     <div className={styles.page}>
       <div className={styles.main}>
         <h2>Login</h2>
-        <form onSubmit={verificarLogin}>
+        <form onSubmit={onSubmit}>
 
           <input type="text" 
            onChange={(e) => setEmail(e.target.value)}
@@ -32,7 +62,8 @@ export default function Login() {
            onChange={(e) => setPassword(e.target.value)}
            placeholder="Senha" required className={styles.input}/>
 
-          <p style={{color: "red"}}>{error}</p>
+          {error && <p style={{color: "red"}}>{error}</p>}
+
           <button type="submit" className={styles.link}>Login</button> 
         </form>
       </div>
