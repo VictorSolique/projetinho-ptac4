2ebb04d07e0ec8d9b014d70cd680fcdb40243c9b
Victor Solique <victordesouzasolique@gmail.com>
2024-11-07 06:59:36 -0400

    commit 07/11

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,18 +10,25 @@ export default function Login() {
   const [error, setError] = useState<string>();
   const [usuarios, setUsuarios] = useState<Usuario[]>([
     {
-        id: 1,
-        nome: "Jorginho BeachTenis",
-        email: "jorginho.beachtenis@gmail.com",
-        password: "senha123",
-        tipo: "adm"
+      id: 1,
+      nome: "Jorginho BeachTenis",
+      email: "jorginho.beachtenis@gmail.com",
+      password: "senha123",
+      tipo: "adm"
     },
     {
-        id: 2,
-        nome: "Cela Lopes",
-        email: "cela.lopes@gmail.com",
-        password: "senha123",
-        tipo: "cliente"
+      id: 2,
+      nome: "Cela Lopes",
+      email: "cela.lopes@gmail.com",
+      password: "senha123",
+      tipo: "cliente"
+    },
+    {
+      id: 2,
+      nome: "Usuario",
+      email: "usuario@gmail.com",
+      password: "123456",
+      tipo: "cliente"
     }
   ])
   const router = useRouter();
@@ -29,14 +36,14 @@ export default function Login() {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const usuario = usuarios.find((user) => user.email == email && user.password == password)
-    if(usuario) {
+    if (usuario) {
       localStorage.setItem('usuario', JSON.stringify(usuario))
       router.push('/home')
     }
     else {
       setError("Email ou senha invalidos")
     }
-    
+
   }
 
   useEffect(() => {
@@ -50,24 +57,44 @@ export default function Login() {
   }, [router])
 
   return (
-    <div className={styles.page}>
-      <div className={styles.main}>
-        <h2>Login</h2>
-        <form onSubmit={onSubmit}>
+    <section className="vh-100" style={{ backgroundColor: "#508bfc" }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card shadow-2-strong" style={{ borderRadius: "1rem" }}>
+              <div className="card-body p-5 text-center">
 
-          <input type="text" 
-           onChange={(e) => setEmail(e.target.value)}
-           placeholder="Email" required className={styles.input}/>
-          <input type="password" 
-           onChange={(e) => setPassword(e.target.value)}
-           placeholder="Senha" required className={styles.input}/>
+                <h3 className="mb-5">Fazer login</h3>
 
-          {error && <p style={{color: "red"}}>{error}</p>}
+                <form onSubmit={onSubmit}>
 
-          <button type="submit" className={styles.link}>Login</button> 
-        </form>
+                  <div data-mdb-input-init className="form-outline mb-3">
+                    <input type="email" id="typeEmail-2"
+                      onChange={(e) => setEmail(e.target.value)} className="form-control form-control-lg" required />
+                    <label className="form-label" htmlFor="typeEmailX-2">Email</label>
+                  </div>
+
+                  <div data-mdb-input-init className="form-outline mb-3">
+                    <input type="password" id="typePasswordX-2"
+                      onChange={(e) => setPassword(e.target.value)} className="form-control form-control-lg" required />
+                    <label className="form-label" htmlFor="typePasswordX-2">Password</label>
+                  </div>
+
+                  {error &&
+                    <div className="d-flex justify-content-center mb-2">
+                      <p className="text-danger"> {error} </p>
+                    </div>
+                  }
+
+
+                  <button type="submit" className="btn btn-primary btn-lg btn-block">Acessar cadastro</button>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-
+    </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,100 @@
+'use client'
 import styles from "./page.module.css"
 import Link from "next/link";
 import Button from "./components/Button";
 import MyInput from "./components/Input";
+import { AUTOMATIC_FONT_OPTIMIZATION_MANIFEST } from "next/dist/shared/lib/constants";
 
 export default function Home() {
   return (
-    <div className={styles.main}>
+    <div>
+
+    <div className="container">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Eleventh navbar example">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExample09">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form role="search">
+            <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <nav className="navbar navbar-expand-lg bg-body-tertiary rounded my-3" aria-label="Twelfth navbar example">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Centered nav only</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div>
+      <div className="bg-body-tertiaryp-5 rounded">
+        <div className="col-sm-8 mx-auto">
+          <h1>Navbar examples</h1>
+          <p>This example is a quick exercise to illustrate how the navbar and its contents work. Some navbars extend the width of the viewport, others are confined within a <code>.container</code>. For positioning of navbars, checkout the <a href="../examples/navbar-static/">top</a> and <a href="../examples/navbar-fixed/">fixed top</a> examples.</p>
+          <p>At the smallest breakpoint, the collapse plugin is used to hide the links and show a menu button to toggle the collapsed content.</p>
+          <p>
+            <a className="btn btn-primary" href="../components/navbar/" role="button">View navbar docs &raquo;</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
         <h1>Login Necessário</h1>
         <p>Realize o login para poder acessar cadastro</p>
         <Link href={"/login"} className={styles.link}>Ir para login</Link>
